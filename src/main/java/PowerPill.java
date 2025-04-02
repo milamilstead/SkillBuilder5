4c48746c850d8ff8a8885732bd16c17f3565453d
milamilstead <mila.milstead@gmail.com>
2025-04-01 23:15:23 -0400

    COMPLETE!!

--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -9,30 +9,54 @@
  */
 public class PowerPill
 {
-    // TODO - replace this line with instruction from step 1
+    public static int DEFAULT_POWER = 10;
 
     // instance variables
-    // TODO - replace this line with instruction from step 2
-    // TODO - replace this line with instruction from step 3
+    int power;
+    String name;
 
     // constructors
-
-    // TODO - replace this line with instruction from step 4
-
-    // TODO - replace this line with instruction from step 5
-
-
+    /**
+     \* Initializes this power pill to a default power value
+     \* and sets the name of the pill to name.
+     \* @param name the name of this power pill.
+     */
+    public PowerPill(String name){
+       this.name = name;
+       this.power = DEFAULT_POWER;
+    }
+    /**
+     * Initializes this power pill to the value of power
+     * and sets the name of the pill to name.
+     * @param name the name of this power pill
+     * @param power the power level of this power pill.
+     */
+    public PowerPill(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
     // accessor methods
-
-    // TODO - replace this line with instruction from step 6
-
+    public String getName() {
+        return name;
+    }
+    public int getPower(){
+        return power;
+    }
 
     // mutator methods
 
-    // TODO - replace this line with instruction from step 7
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setPower(int power){
+        this.power = power;
+    }
 
     // toString method
 
-    // TODO - replace this line with instruction from step 8
+    public String toString(){
+        return "PowerPill" + name + " = " + power;
+    }
 
 }
